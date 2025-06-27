14daf1a3aa60e1593e74708e4e5421a055bca565
Uyonoh <68875749+Uyonoh@users.noreply.github.com>
2025-06-27 11:48:49 +0100

    Update build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -5,6 +5,8 @@ set -o errexit
 #pip install uv
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
+uv venv .venv
+source .venv/bin/activate
 
 uv pip install -r requirements.txt
 
