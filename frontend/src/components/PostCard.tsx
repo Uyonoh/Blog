9581840389a0a92ec1af086ae62b646def8ba2aa
Uyonoh <turakiuyonoh@gmail.com>
2025-11-03 08:47:08 +0100

    bug: fix post list view

--- frontend/src/components/PostCard.tsx
+++ frontend/src/components/PostCard.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 // Define a more specific type for the post
 // type Author = {
