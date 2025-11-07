2db0e159a52b5e7dfd0feea0b719598afc5f6c31
Uyonoh <turakiuyonoh@gmail.com>
2025-11-07 20:34:29 +0100

    bug: fix dark mode toggle icons

--- frontend/src/components/DarkModeToggle.tsx
+++ frontend/src/components/DarkModeToggle.tsx
@@ -18,7 +18,7 @@ export default function DarkModeToggle() {
 
   return (
     <button onClick={toggleTheme} className="p-2 rounded-full bg-gray-200 dark:bg-gray-700">
-          {theme === "light" ? <Sun className="w-5 h-5 text-yellow-400" /> : <Moon className="w-5 h-5 text-gray-800" />}
+          {theme === "light" ? <Moon className="w-5 h-5 text-gray-800" fill='gray-800' /> : <Sun className="w-5 h-5 text-yellow-400" fill='yellow' />}
     </button>
   );
 }
