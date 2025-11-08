402fe2cc7d68978760494c274366f441c5113d68
Uyonoh <turakiuyonoh@gmail.com>
2025-11-08 09:09:25 +0100

    modify next config: add remote patterbs

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,16 +1,29 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: ["localhost", "127.0.0.1"],
   images: {
-    domains: ["localhost", "uyonoh.com", "127.0.0.1"],
-    // remotePatterns: [
-    //   {
-    //     protocol: 'http',
-    //     hostname: 'localhost',
-    //     port: '8000',
-    //     pathname: '/media/**',
-    //   },
-    // ],
+    // domains: ["localhost", "uyonoh.com", "127.0.0.1"],
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '8000',
+        pathname: '/media/**',
+      },
+      {
+        protocol: 'http',
+        hostname: '127.0.0.1',
+        port: '8000',
+        pathname: '/media/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'uyonoh.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
 };
 
