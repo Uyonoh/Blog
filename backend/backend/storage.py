0285baf970bd0ec6ff061091d6b27bf8f9cf3ef8
Uyonoh <turakiuyonoh@gmail.com>
2026-03-08 13:51:52 +0100

    use manifeststatic in place of super

--- backend/backend/storage.py
+++ backend/backend/storage.py
@@ -1,5 +1,5 @@
 # Custom whitenoise storage
-from whitenoise.storage import CompressedManifestStaticFilesStorage
+from whitenoise.storage import ManifestStaticFilesStorage,CompressedManifestStaticFilesStorage
 from whitenoise.storage import MissingFileError
 
 
@@ -7,15 +7,20 @@ class IgnoreMissingManifestStorage(CompressedManifestStaticFilesStorage):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+    def process_missing_files(self, files):
+        for name, hashed_name, processed in files:
+            if isinstance(processed, MissingFileError):
+                # Skip if missing
+                continue
+            yield name, hashed_name, processed
+
     def post_process(self, *args, **kwargs):
-        files = super().post_process(*args, **kwargs)
+        files = ManifestStaticFilesStorage.post_process(*args, **kwargs)
+        files = self.process_missing_files(files)
 
         if not kwargs.get("dry_run"):
             files = self.post_process_with_compression(files)
 
         # Make exception messages helpful
         for name, hashed_name, processed in files:
-            if isinstance(processed, MissingFileError):
-                # Skip if missing
-                continue
             yield name, hashed_name, processed
