a52bdcb3b4246bda8f450e7e10fcf6596943990f
Uyonoh <turakiuyonoh@gmail.com>
2026-03-08 13:25:27 +0100

    modify custom storage

--- backend/backend/storage.py
+++ backend/backend/storage.py
@@ -1,12 +1,20 @@
 # Custom whitenoise storage
 from whitenoise.storage import CompressedManifestStaticFilesStorage
+from whitenoise.storage import MissingFileError
 
 
 class IgnoreMissingManifestStorage(CompressedManifestStaticFilesStorage):
-    def url(self, name, **kwargs):
-        try:
-            return super().url(name, **kwargs)
-        except (ValueError, FileNotFoundError):
-            # If the file (like the .map file) is missing, 
-            # just return the original name instead of crashing
-            return name
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+    def post_process(self, *args, **kwargs):
+        files = super().post_process(*args, **kwargs)
+
+        if not kwargs.get("dry_run"):
+            files = self.post_process_with_compression(files)
+
+        # Make exception messages helpful
+        for name, hashed_name, processed in files:
+            if isinstance(processed, MissingFileError):
+                processed = True
+            yield name, hashed_name, processed
