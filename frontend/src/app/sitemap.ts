7af0da652ac1e2b303e0463ae4469bbb81b9b094
Uyonoh <turakiuyonoh@gmail.com>
2026-08-09 14:10:35 +0100

    fix: adjust canonical and sitemap url. modify structured data

--- frontend/src/app/sitemap.ts
+++ frontend/src/app/sitemap.ts
@@ -2,19 +2,26 @@ import { MetadataRoute } from 'next';
 import { getAllPostSlugs } from '@/lib/data';
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const baseUrl = 'https://blog.uyonoh.com';
+  const domainUrl = 'https://uyonoh.com/';
+  const baseUrl = 'https://blog.uyonoh.com/';
   
   // Fetch all post slugs
   const slugs = await getAllPostSlugs();
   
   const postEntries = slugs.map((slug) => ({
-    url: `${baseUrl}/post/${slug}`,
+    url: `${baseUrl}post/${slug}`,
     lastModified: new Date(),
     changeFrequency: 'weekly' as const,
     priority: 0.8,
   }));
 
   return [
+    {
+      url: domainUrl,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
     {
       url: baseUrl,
       lastModified: new Date(),
